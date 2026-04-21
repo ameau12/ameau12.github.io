18cb49b5dfc453bed56e51637ad5d40f9d2f1d4e
ameau12 <ameau12@lsu.edu>
2026-04-21 14:20:11 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,24 +1,34 @@
+// Create a new map centered on the continental US
 var map = L.map('map').setView([38, -95], 4);
+
+// Add OpenStreetMap to the map
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
+
+// Add weather radar to the map
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
   transparent: true
 };
+
+// Get GeoJSON data from the NWS weather alerts API
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
   L.geoJSON(data, {
-    style: {color: 'orange' },
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup(feature.properties.headline);
-    }
+    // Color all alert polygons orange, but color Severe polygons red
     style: function(feature){
       var alertColor = 'orange';
       if (feature.properties.severity === 'Severe') alertColor = 'red';
+      if (feature.properties.severity === 'Extreme') alertColor = 'indianred';
+      if (feature.properties.severity === 'Minor') alertColor = 'darkkhaki';
       return { color: alertColor };
     },
+    // Add a popup on each feature showing the NWS alert headline
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(feature.properties.headline);
+    }
   }).addTo(map);
 });
