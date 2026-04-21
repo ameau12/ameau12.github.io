029bdbd26aa9f9fdc82c7a2ec2de1990e5caa5cf
ameau12 <ameau12@lsu.edu>
2026-04-21 14:43:41 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -2,7 +2,7 @@
 var map = L.map('map').setView([38, -95], 4);
 
 // Add OpenStreetMap to the map
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var basemapUrl = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
 
 // Add weather radar to the map
